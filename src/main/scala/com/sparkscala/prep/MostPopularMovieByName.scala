6ee31df35ec065d5afbaa5f58f8879b3a325b68c
nhareesha <hareesha1906@gmail.com>
2019-01-02 22:52:14 -0800

    MostPopular movie by name - broadcast variable

--- src/main/scala/com/sparkscala/prep/MostPopularMovieByName.scala
+++ src/main/scala/com/sparkscala/prep/MostPopularMovieByName.scala
@@ -2,6 +2,8 @@ package com.sparkscala.prep
 
 import java.nio.charset.CodingErrorAction
 
+import org.apache.spark.SparkContext
+
 import scala.io.{Codec, Source}
 
 /**
@@ -20,12 +22,39 @@ object MostPopularMovieByName {
     var movieNames: Map[Int, String] = Map()
     val lines = Source.fromFile("../ml-100k/u.item").getLines()
     for (line <- lines) {
-      val lineVals = line.split("|")
+      var lineVals = line.split('|')
       if (lineVals.length > 1) {
         movieNames += (lineVals(0).toInt -> lineVals(1))
       }
     }
-    movieNames
+    return movieNames
+  }
+
+  def main(args: Array[String]):Unit = {
+    val sc = new SparkContext("local[*]","PopularMovieByName")
+
+    // broadcast movieId map
+    // Doing this, the result of loadMovieNames is available to all the executors
+    var bcMovieNames = sc.broadcast(loadMovieNames)
+
+    val movieInfoRdd = sc.textFile("../ml-100k/u.data")
+
+    // from movieInfoRdd map to (movieId,1)
+    val movieIdRdd = movieInfoRdd.map(x => (x.split("\t")(1).toInt , 1))
+
+    val popularMovieRdd = movieIdRdd.reduceByKey((v1,v2) => v1+v2)
+
+    // map (MovieId, count) to (count, movieId)
+    val flipped = popularMovieRdd.map(x => (x._2,x._1))
+
+    // sort (count,movieId) tuples based on the key count
+    val sortedPopularMovieIds = flipped.sortByKey();
+
+    // map (count.movieId) to get (movieName, count) - Use broadcasted map as lookup data to get movieName for given movieId
+    val popularMoviesByName = sortedPopularMovieIds.map(x => (bcMovieNames.value.getOrElse(x._2,null) ,x._1))
+
+    // collect and print popular movies
+    popularMoviesByName.collect().foreach(println)
   }
 
 }
